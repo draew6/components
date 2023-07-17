35555da8c8e98228761a30b55971ca8189f0c66b
draew6 <adosko6@gmail.com>
2023-07-17 18:06:21 +0200

    REMOVED: button width

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import type {Writable} from 'svelte/store'
-    type tailwindNumbers = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 14 | 16 | 20 | 24 | 28 | 32 | 36 | 40 | 44 | 48 | 52 | 56 | 60 | 64 | 72 | 80 | 96
 
     export let text = ''
     export let isLoading = false
@@ -8,8 +7,6 @@
     export let handler: Function
     export let additionClass = ''
     export let color = 'btn-primary'
-
-    export let width: tailwindNumbers = null
     
     switch (color) {
         case 'green':
@@ -32,21 +29,20 @@
         isLoading = true
         if (loadStore) loadStore.set(true)
         await handler()
-        isLoading = false
         if (loadStore) loadStore.set(false)
+        isLoading = false
     }
 
-
 </script>
-
 <button
-    class="btn {color} {additionClass} {width === null ? '' : 'w-' + width}"
-    on:click={handle}
-    >
+    class="btn {color} {additionClass}"
+    on:click={isLoading ? () => {} : handle}
+    >{#if isLoading}
         <span class:spin={isLoading}></span>
+    {:else}
         {text}
+    {/if}
 </button>
-
 <style>
     .spin {
         @apply loading loading-spinner;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
   import Button from '$lib/Button.svelte'
+
+  async function delay() {
+    console.log("started")
+  await new Promise(resolve => setTimeout(resolve, 10000));
+  console.log("Delayed for 10 seconds");
+}
 </script>
-<Button handler={()=> console.log("triggered!")} width={56}/>
+<Button handler={delay} text="TEXTT" color="blue" additionClass={"w-96"}/>
 <style lang="postcss">
     :global(html) {
       background-color: theme(colors.gray.100);
