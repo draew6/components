aaee60b49d8c3e66049b85786a1e4f7178aae4b6
draew6 <adosko6@gmail.com>
2023-08-05 15:52:37 +0200

    changed margin from top to bottom

--- src/lib/PasswordInput.svelte
+++ src/lib/PasswordInput.svelte
@@ -3,4 +3,4 @@
     export let value = ''
     export let disabled = false
 </script>
-<input type="password" bind:value {placeholder} class="input input-bordered w-full max-w-xs py-2 px-3 mt-4 disabled:text-gray-500" {disabled}/>
+<input type="password" bind:value {placeholder} class="input input-bordered w-full max-w-xs py-2 px-3 mb-4 disabled:text-gray-500" {disabled}/>
