f96aeb0322920d7122219a407f71fbf4d1195bf3
draew6 <adosko6@gmail.com>
2023-08-05 13:39:41 +0200

    added gray text for disabled input

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -2,4 +2,4 @@
     export let placeholder = ''
     export let value = ''
 </script>
-<input type="text" bind:value {placeholder} class="input input-bordered w-full max-w-xs py-2 px-3 mt-4" />
+<input type="text" bind:value {placeholder} class="input input-bordered w-full max-w-xs py-2 px-3 mt-4 disabled:text-gray-500" />
